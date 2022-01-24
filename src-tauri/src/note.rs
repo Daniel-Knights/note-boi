1fd8f641ca38e53236fd4f6295e7ff02e3e6bb9a
Dan Knights <daniel.knights@28b.co.uk>
2022-01-24 23:06:06 +0000

    refactor: rustify functions

--- src-tauri/src/note.rs
+++ src-tauri/src/note.rs
@@ -1,6 +1,7 @@
 use chrono::{DateTime, Local};
 use serde::{Deserialize, Serialize};
 use std::fs;
+use std::io::Write;
 use std::path::{Path, PathBuf};
 use std::time::SystemTime;
 use uuid::Uuid;
@@ -24,27 +25,6 @@ pub enum NoteError {
   UnableToEditFile(String),
 }
 
-fn get_path(id: &String, with_ext: bool) -> PathBuf {
-  let path_str = ".notes/".to_owned() + id;
-
-  match with_ext {
-    true => PathBuf::from(path_str + ".json"),
-    false => PathBuf::from(path_str),
-  }
-}
-
-fn format_date(time: SystemTime) -> String {
-  DateTime::<Local>::from(time).format("%c").to_string()
-}
-
-fn serialize_note(note: &Note) -> String {
-  serde_json::to_string(note).expect("unable to serialize note struct")
-}
-
-fn deserialize_note(note_json: &String) -> Note {
-  serde_json::from_str::<Note>(note_json).expect("unable to deserialize note json")
-}
-
 impl Note {
   fn new(path: &PathBuf, id: String, title: String, body: String) -> Note {
     let metadata = path.metadata().expect("unable to get metadata");
@@ -55,9 +35,9 @@ impl Note {
       id: Uuid::parse_str(&id).expect("unable to parse id as uuid"),
       title,
       body,
-      created_at: format_date(created),
-      updated_at: format_date(modified),
-      modified_timestamp: DateTime::<Local>::from(modified).timestamp(),
+      created_at: created.to_date_format(),
+      updated_at: modified.to_date_format(),
+      modified_timestamp: modified.to_date_time().timestamp(),
     }
   }
 
@@ -69,13 +49,12 @@ impl Note {
     }
 
     let id = Uuid::new_v4().to_string();
-    let path = get_path(&id, true);
-
-    fs::write(&path, "").unwrap();
+    let path = Note::get_path(&id, true);
+    let mut file = fs::File::create(&path).unwrap();
 
     let note = Note::new(&path, id, title, body);
-    let note_json = serialize_note(&note);
-    let write_res = fs::write(path, note_json);
+    let note_json = note.serialize();
+    let write_res = file.write(note_json.as_ref());
 
     match write_res {
       Ok(_) => Ok(note),
@@ -101,17 +80,17 @@ impl Note {
   }
 
   pub fn get(id: String) -> Result<Note, NoteError> {
-    let path = get_path(&id, true);
+    let path = Note::get_path(&id, true);
     let read_res = fs::read_to_string(&path);
 
     match read_res {
-      Ok(note_json) => Ok(deserialize_note(&note_json)),
+      Ok(note_json) => Ok(Note::deserialize(&note_json)),
       Err(e) => Err(NoteError::UnableToGetFile(e.to_string())),
     }
   }
 
   pub fn delete(id: String) -> Result<bool, NoteError> {
-    let path = get_path(&id, true);
+    let path = Note::get_path(&id, true);
     let delete_res = fs::remove_file(path);
 
     match delete_res {
@@ -121,9 +100,9 @@ impl Note {
   }
 
   pub fn edit(id: String, title: String, body: String) -> Result<Note, NoteError> {
-    let path = get_path(&id, true);
+    let path = Note::get_path(&id, true);
     let note = Note::new(&path, id, title, body);
-    let note_json = serialize_note(&note);
+    let note_json = note.serialize();
     let write_res = fs::write(path, note_json);
 
     match write_res {
@@ -131,14 +110,45 @@ impl Note {
       Err(e) => Err(NoteError::UnableToEditFile(e.to_string())),
     }
   }
+
+  fn get_path(id: &String, with_ext: bool) -> PathBuf {
+    let mut path_str = ".notes/".to_owned() + id;
+
+    if with_ext {
+      path_str.push_str(".json");
+    }
+
+    PathBuf::from(path_str)
+  }
+
+  fn serialize(&self) -> String {
+    serde_json::to_string(self).expect("unable to serialize note struct")
+  }
+  fn deserialize(note_json: &String) -> Note {
+    serde_json::from_str::<Note>(note_json).expect("unable to deserialize note json")
+  }
 }
 
 impl From<fs::DirEntry> for Note {
   fn from(entry: fs::DirEntry) -> Note {
     let file_name = entry.file_name().into_string().unwrap();
-    let path = get_path(&file_name, false);
+    let path = Note::get_path(&file_name, false);
     let note_json = fs::read_to_string(path).expect("unable to read file");
 
-    deserialize_note(&note_json)
+    Note::deserialize(&note_json)
+  }
+}
+
+trait ToDate {
+  fn to_date_time(&self) -> DateTime<Local>;
+  fn to_date_format(&self) -> String;
+}
+
+impl ToDate for SystemTime {
+  fn to_date_time(&self) -> DateTime<Local> {
+    DateTime::<Local>::from(*self)
+  }
+  fn to_date_format(&self) -> String {
+    self.to_date_time().format("%c").to_string()
   }
 }
