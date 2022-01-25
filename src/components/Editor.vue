3c66dbdc23f549b71a56a1f12f3c507e28899ff6
Dan Knights <daniel.knights@28b.co.uk>
2022-01-25 22:07:15 +0000

    feat: editor styling

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="editor">
-    <h1 contenteditable="true">{{ state.selectedNote?.title }}</h1>
-    <pre contenteditable="true">{{ state.selectedNote?.body }}</pre>
+    <h1 class="editor__title" contenteditable="true">{{ state.selectedNote?.title }}</h1>
+    <div class="editor__dates">
+      <small>Created: {{ state.selectedNote?.created_at }}</small>
+      <small>Updated: {{ state.selectedNote?.updated_at }}</small>
+    </div>
+    <pre class="editor__body" contenteditable="true">{{ state.selectedNote?.body }}</pre>
   </div>
 </template>
 
@@ -19,20 +23,41 @@ import { state } from '../store';
   }
 }
 
-h1,
-pre {
+.editor__title,
+.editor__body,
+.editor__dates {
+  padding-left: 8px;
+  padding-right: 8px;
+}
+
+.editor__title,
+.editor__body {
   outline: none;
 }
 
-h1 {
-  font-size: 2rem;
+.editor__title {
+  font-size: 32px;
   font-weight: bold;
-  padding: 1rem 0;
-  border-bottom: 1px solid;
+  padding-top: 8px;
 }
 
-pre {
-  margin-top: 1rem;
+.editor__dates {
+  padding-top: 16px;
+  padding-bottom: 16px;
+  font-size: 11px;
+  border-bottom: 1px solid;
+
+  small {
+    display: block;
+
+    + small {
+      margin-top: 5px;
+    }
+  }
+}
+
+.editor__body {
+  padding-top: 16px;
   height: 100%;
 }
 </style>
