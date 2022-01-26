d26de33e4aaa46d4720410db1a8780a0a2f8f82b
Dan Knights <daniel.knights@28b.co.uk>
2022-01-26 07:30:54 +0000

    ci: change name to note-boi

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Notes
+# NoteBoi
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,8 +7,19 @@ mod command;
 mod note;
 use crate::command::{delete_note, edit_note, get_all_notes, get_note, new_note};
 
+use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
+
 fn main() {
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let close = CustomMenuItem::new("close".to_string(), "Close");
+  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
+  let menu = Menu::new()
+    .add_native_item(MenuItem::Copy)
+    .add_item(CustomMenuItem::new("hide", "Hide"))
+    .add_submenu(submenu);
+
   tauri::Builder::default()
+    .menu(menu)
     .invoke_handler(tauri::generate_handler![
       delete_note,
       edit_note,
