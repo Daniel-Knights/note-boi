c7eba7703cc991108753fbacda0a00394a453b2c
Daniel Knights <danknights95@gmail.com>
2022-01-26 15:33:09 +0000

    refactor: move menu functionality to module

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,38 +4,15 @@
 )]
 
 mod command;
+mod menu;
 mod note;
+
 use crate::command::{delete_note, edit_note, get_all_notes, new_note};
 
-use tauri::{CustomMenuItem, Menu, Submenu};
-
 fn main() {
-  let about = CustomMenuItem::new("about".to_string(), "About");
-  let preferences = CustomMenuItem::new("preferences".to_string(), "Preferences");
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  let submenu = Submenu::new(
-    "File",
-    Menu::new()
-      .add_item(about)
-      .add_item(preferences)
-      .add_item(quit),
-  );
-  let menu = Menu::new().add_submenu(submenu);
-
   tauri::Builder::default()
-    .menu(menu)
-    .on_menu_event(|event| match event.menu_item_id() {
-      "about" => {
-        todo!();
-      }
-      "preferences" => {
-        todo!();
-      }
-      "quit" => {
-        std::process::exit(0);
-      }
-      _ => {}
-    })
+    .menu(menu::get_menu())
+    .on_menu_event(menu::handle_event)
     .invoke_handler(tauri::generate_handler![
       delete_note,
       edit_note,
