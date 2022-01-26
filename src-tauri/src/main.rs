6cfdb4c2a4b7d11e2d95c063b2df7441d663c668
Dan Knights <daniel.knights@28b.co.uk>
2022-01-26 08:13:32 +0000

    feat: multi-window setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,15 +10,32 @@ use crate::command::{delete_note, edit_note, get_all_notes, new_note};
 use tauri::{CustomMenuItem, Menu, Submenu};
 
 fn main() {
+  let about = CustomMenuItem::new("about".to_string(), "About");
+  let preferences = CustomMenuItem::new("preferences".to_string(), "Preferences");
   let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  let close = CustomMenuItem::new("close".to_string(), "Close");
-  let submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close));
-  let menu = Menu::new()
-    .add_item(CustomMenuItem::new("hide", "Hide"))
-    .add_submenu(submenu);
+  let submenu = Submenu::new(
+    "File",
+    Menu::new()
+      .add_item(about)
+      .add_item(preferences)
+      .add_item(quit),
+  );
+  let menu = Menu::new().add_submenu(submenu);
 
   tauri::Builder::default()
     .menu(menu)
+    .on_menu_event(|event| match event.menu_item_id() {
+      "about" => {
+        todo!();
+      }
+      "preferences" => {
+        todo!();
+      }
+      "quit" => {
+        std::process::exit(0);
+      }
+      _ => {}
+    })
     .invoke_handler(tauri::generate_handler![
       delete_note,
       edit_note,
