bdbc57a49ec803c27bdadd5446b18c376d78b328
Daniel Knights <danknights95@gmail.com>
2022-02-13 22:07:45 +0000

    feat: use tauri-plugin-window-state

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,14 +4,10 @@
 )]
 
 mod command;
-mod config;
 mod note;
-mod window;
 
 use crate::command::{delete_note, edit_note, get_all_notes, new_note};
-use crate::config::Config;
 use tauri::{WindowBuilder, WindowUrl};
-use window::WindowState;
 
 fn main() {
   tauri::Builder::default()
@@ -22,24 +18,17 @@ fn main() {
       new_note
     ])
     .create_window("main", WindowUrl::App("".into()), |win, attr| {
-      let config = Config::get();
-
       (
         win
           .title("NoteBoi")
-          .inner_size(config.window_state.size.0, config.window_state.size.1)
           .min_inner_size(600.0, 400.0)
-          .position(
-            config.window_state.position.0,
-            config.window_state.position.1,
-          )
           .resizable(true)
           .fullscreen(false)
-          .always_on_top(true),
+          .visible(false),
         attr,
       )
     })
-    .on_window_event(|ev| WindowState::handle_event(ev))
+    .plugin(tauri_plugin_window_state::WindowState::default())
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
