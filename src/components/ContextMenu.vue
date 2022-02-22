f1bf9f0fcaad500559ce0029ebb86b562a2a2925
Dan Knights <daniel.knights@28b.co.uk>
2022-02-22 19:07:27 +0000

    feat(context menu): flesh out styling

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -2,9 +2,9 @@
   <ul class="context-menu" :style="{ top: top + 'px', left: left + 'px' }">
     <li>New Note</li>
     <li>Delete Note</li>
-    <li>
+    <li class="context-menu__has-sub-menu">
       Theme
-      <ul class="context-menu__sub-menu">
+      <ul>
         <li>Light</li>
         <li>Dark</li>
         <li>System</li>
@@ -29,18 +29,42 @@ defineProps({
 </script>
 
 <style lang="scss" scoped>
-.context-menu {
+$list-padding: 3px;
+$list-bg-color: var(--color__interactive);
+
+ul {
   position: absolute;
-  background-color: var(--color__primary);
+  color: var(--color__secondary);
+  background-color: $list-bg-color;
   z-index: 10;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+
+  li {
+    cursor: pointer;
+    padding: 0.5em 1em;
+    white-space: nowrap;
+    border: $list-padding solid $list-bg-color;
+
+    &:hover {
+      color: var(--color__primary);
+      background-color: var(--color__tertiary);
+    }
+  }
 }
 
-.context-menu__sub-menu {
-  display: none;
-  position: absolute;
-  top: 0;
-  left: 100%;
-  background-color: var(--color__primary);
-  z-index: 10;
+.context-menu__has-sub-menu {
+  position: relative;
+
+  &:hover {
+    > ul {
+      display: block;
+    }
+  }
+
+  > ul {
+    display: none;
+    top: 0;
+    left: calc(100% + $list-padding);
+  }
 }
 </style>
