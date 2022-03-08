20cd5b7f36c85db0042d1af4fb4988710389bff6
Dan Knights <daniel.knights@28b.co.uk>
2022-03-08 09:34:19 +0000

    feat: adjust colour themes

--- src/components/svg/PlusIcon.vue
+++ src/components/svg/PlusIcon.vue
@@ -1,5 +1,8 @@
 <template>
   <svg viewBox="0 0 64 64" height="16">
-    <path fill="#fff" d="M36.74,0V27.26H64v9.48H36.74V64H27.26V36.74H0V27.26H27.26V0Z" />
+    <path
+      fill="var(--white)"
+      d="M36.74,0V27.26H64v9.48H36.74V64H27.26V36.74H0V27.26H27.26V0Z"
+    />
   </svg>
 </template>
