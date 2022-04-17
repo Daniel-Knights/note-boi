49be81215b2d8bea42d8723f0a795b5aff21c052
Dan Knights <daniel.knights@28b.co.uk>
2022-04-17 09:00:54 +0100

    ci(tests): start of contextmenu tests

--- src/__tests__/components/ContextMenu.test.ts
+++ src/__tests__/components/ContextMenu.test.ts
@@ -1,12 +1,30 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, assert } from 'vitest';
 import { mount } from '@vue/test-utils';
 
 import ContextMenu from '../../components/ContextMenu.vue';
 
 describe('ContextMenu', () => {
-  it('Mounts', () => {
+  it('Mounts without passed ev', () => {
     const wrapper = mount(ContextMenu);
 
-    expect(wrapper).toBeDefined();
+    assert.isDefined(wrapper);
+    assert.isFalse(wrapper.isVisible());
+  });
+
+  it('Mounts with ev', async () => {
+    const ev: MouseEvent = new MouseEvent('contextmenu', {
+      clientX: 100,
+      clientY: 200,
+    });
+
+    const wrapper = mount(ContextMenu);
+    await wrapper.setProps({ ev: ev || undefined });
+
+    const elementStyles = (wrapper.element as HTMLUListElement).style;
+
+    assert.isDefined(wrapper);
+    assert.isTrue(wrapper.isVisible());
+    assert.strictEqual(elementStyles.top, `${ev.clientY}px`);
+    assert.strictEqual(elementStyles.left, `${ev.clientX}px`);
   });
 });
