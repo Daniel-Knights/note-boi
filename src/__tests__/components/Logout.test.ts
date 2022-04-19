d45374c918e8eff6196b87a31dd9a4751d5731ba
Dan Knights <daniel.knights@28b.co.uk>
2022-04-19 21:23:47 +0100

    ci(tests): logout

--- src/__tests__/components/Logout.test.ts
+++ src/__tests__/components/Logout.test.ts
@@ -1,11 +1,46 @@
+import { nextTick } from 'vue';
 import { mount } from '@vue/test-utils';
 
 import Logout from '../../components/Logout.vue';
+import { mockTauriApi } from '../tauri';
+import { setCrypto } from '../utils';
+import * as syncStore from '../../store/sync';
+import localNotes from '../notes.json';
 
-describe.todo('Logout', () => {
+beforeAll(setCrypto);
+
+describe('Logout', () => {
   it('Mounts', () => {
     const wrapper = mount(Logout);
 
-    expect(wrapper).toBeDefined();
+    assert.isDefined(wrapper);
+    assert.isFalse(wrapper.isVisible());
+  });
+
+  it('Logs out on click', async () => {
+    mockTauriApi(localNotes);
+    const wrapper = mount(Logout);
+
+    assert.isDefined(wrapper);
+    assert.isFalse(wrapper.isVisible());
+
+    syncStore.state.username = 'd';
+    syncStore.state.password = '1';
+    await syncStore.login();
+
+    assert.isTrue(wrapper.isVisible());
+
+    await wrapper.trigger('click');
+
+    // Workaround for awaiting logout call
+    function awaitLogout(): Promise<void> | void {
+      if (syncStore.state.isLoading) {
+        return nextTick().then(awaitLogout);
+      }
+    }
+
+    await awaitLogout();
+
+    assert.isEmpty(syncStore.state.token);
   });
 });
