2104ecefe93f8899a3f14ada481852801281fe31
Dan Knights <daniel.knights@28b.co.uk>
2022-05-17 19:52:29 +0100

    fix(version): bump tauri conf version before package

--- scripts/version.mjs
+++ scripts/version.mjs
@@ -7,7 +7,27 @@ const packageData = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
 const tauriConfPath = path.join('.', 'src-tauri', 'tauri.conf.json');
 const tauriConf = JSON.parse(fs.readFileSync(tauriConfPath, 'utf8'));
 
-tauriConf.package.version = packageData.version;
+const versionSections = packageData.version.split('.');
+
+switch (process.argv[2]) {
+  case 'major':
+    versionSections[0] = (parseInt(versionSections[0], 10) + 1).toString();
+    versionSections[1] = '0';
+    versionSections[2] = '0';
+    break;
+  case 'minor':
+    versionSections[1] = (parseInt(versionSections[1], 10) + 1).toString();
+    versionSections[2] = '0';
+    break;
+  case 'patch':
+    versionSections[2] = (parseInt(versionSections[2], 10) + 1).toString();
+    break;
+  default:
+    console.error(`Invalid version type: '${process.argv[2]}'`);
+    process.exit(1);
+}
+
+tauriConf.package.version = `${versionSections[0]}.${versionSections[1]}.${versionSections[2]}`;
 
 fs.writeFileSync(tauriConfPath, `${JSON.stringify(tauriConf, null, 2)}\n`);
 
