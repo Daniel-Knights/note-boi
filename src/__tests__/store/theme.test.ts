afb40f30e9eca51a3aa4d4c543220b84d571667a
Daniel Knights <danknights95@gmail.com>
2022-06-12 15:06:17 +0100

    test: theme

---
+++ src/__tests__/store/theme.test.ts
@@ -0,0 +1,17 @@
+import { selectedTheme, setTheme } from '../../store/theme';
+
+describe('Theme', () => {
+  it('Uses system as default', () => {
+    assert.strictEqual(selectedTheme.value, 'System');
+    assert.isTrue(document.body.classList.contains('theme--system'));
+  });
+
+  it('setTheme', () => {
+    setTheme('Light');
+    assert.strictEqual(selectedTheme.value, 'Light');
+    assert.isTrue(document.body.classList.contains('theme--light'));
+    setTheme('Dark');
+    assert.strictEqual(selectedTheme.value, 'Dark');
+    assert.isTrue(document.body.classList.contains('theme--dark'));
+  });
+});
--- src/store/theme.ts
+++ src/store/theme.ts
@@ -3,9 +3,14 @@ import { ref } from 'vue';
 import { STORAGE_KEYS } from '../constant';
 
 export const COLOUR_THEMES = ['Light', 'Dark', 'System'] as const;
-export const selectedTheme = ref(localStorage.getItem(STORAGE_KEYS.THEME) || 'System');
 
-export function setTheme(theme: typeof COLOUR_THEMES[number]): void {
+type ColourTheme = typeof COLOUR_THEMES[number];
+
+export const selectedTheme = ref<ColourTheme>(
+  (localStorage.getItem(STORAGE_KEYS.THEME) as ColourTheme | null) || 'System'
+);
+
+export function setTheme(theme: ColourTheme): void {
   document.body.classList.remove(`theme--${selectedTheme.value.toLowerCase()}`);
   document.body.classList.add(`theme--${theme.toLowerCase()}`);
 
