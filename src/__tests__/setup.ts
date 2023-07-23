3c3673457f5ecb7dfe19fd4542d73fc93710a597
Daniel Knights <daniel.knights@28b.co.uk>
2023-07-23 13:11:43 +0100

    test(setup): spy on assert fail

--- src/__tests__/setup.ts
+++ src/__tests__/setup.ts
@@ -2,11 +2,15 @@ import { enableAutoUnmount } from '@vue/test-utils';
 
 import { resetNoteStore, resetSyncStore, setCrypto } from './utils';
 
+const spyAssertFail = vi.spyOn(assert, 'fail');
+
 beforeAll(() => {
   setCrypto();
 });
 afterEach(() => {
   resetSyncStore();
   resetNoteStore();
+
+  expect(spyAssertFail).not.toHaveBeenCalled();
 });
 enableAutoUnmount(afterEach);
