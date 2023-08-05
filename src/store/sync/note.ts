c3a526cae05d383f4607f5ba00345e84ba7fb5a1
Daniel Knights <daniel.knights@28b.co.uk>
2023-08-05 12:26:19 +0100

    perf(push): return if no unsynced or single empty note

--- src/store/sync/note.ts
+++ src/store/sync/note.ts
@@ -124,6 +124,8 @@ export async function pull(): Promise<void> {
 // Push
 export async function push(isSyncCleanup?: boolean): Promise<void> {
   if (!syncState.token || (syncState.isLoading && !isSyncCleanup)) return;
+  if (unsyncedNoteIds.size === 0) return;
+  if (noteState.notes.length === 1 && isEmptyNote(noteState.notes[0])) return;
 
   syncState.isLoading = true;
 
