3d6785455ff8444d003a33edbdcdbcf7f66aedd1
Daniel Knights <daniel.knights@28b.co.uk>
2023-11-19 21:03:29 +0000

    refactor(menu): simplify window menu event emitting and item toggling

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -45,48 +45,37 @@ pub fn get_menu() -> Menu {
 
 /// Sets the menu to use on all windows and handles any menu events
 pub fn set_menu(builder: Builder<Wry>) -> Builder<Wry> {
-  builder
-    .menu(get_menu())
-    .on_menu_event(|ev| match ev.menu_item_id() {
-      "reload" => ev.window().emit("reload", {}).unwrap(),
-      "new-note" => ev.window().emit("new-note", {}).unwrap(),
-      "export-note" => ev.window().emit("export-note", {}).unwrap(),
-      "export-all-notes" => ev.window().emit("export-all-notes", {}).unwrap(),
-      "delete-note" => ev.window().emit("delete-note", {}).unwrap(),
-      "login" => ev.window().emit("login", {}).unwrap(),
-      "signup" => ev.window().emit("signup", {}).unwrap(),
-      "push-notes" => ev.window().emit("push-notes", {}).unwrap(),
-      "change-password" => ev.window().emit("change-password", {}).unwrap(),
-      "delete-account" => ev.window().emit("delete-account", {}).unwrap(),
-      "logout" => ev.window().emit("logout", {}).unwrap(),
-      _ => {}
-    })
+  let menu = get_menu();
+
+  builder.menu(menu).on_menu_event(|ev| {
+    let id = ev.menu_item_id();
+
+    ev.window().emit(id, {}).unwrap();
+  })
 }
 
 /// Toggles enabled sync menu items
 pub fn toggle_sync_items(app: &mut App<Wry>) {
   let menu_handle = app.get_window("main").unwrap().menu_handle();
 
+  let logged_in_items = vec!["push-notes", "change-password", "delete-account", "logout"];
+  let logged_out_items = vec!["login", "signup"];
+
   let toggle_fn = move |b: bool| {
-    menu_handle.get_item("login").set_enabled(b).unwrap();
-    menu_handle.get_item("signup").set_enabled(b).unwrap();
-    menu_handle.get_item("push-notes").set_enabled(!b).unwrap();
-    menu_handle
-      .get_item("change-password")
-      .set_enabled(!b)
-      .unwrap();
-    menu_handle
-      .get_item("delete-account")
-      .set_enabled(!b)
-      .unwrap();
-    menu_handle.get_item("logout").set_enabled(!b).unwrap();
+    for item in &logged_in_items {
+      menu_handle.get_item(*item).set_enabled(b).unwrap();
+    }
+
+    for item in &logged_out_items {
+      menu_handle.get_item(*item).set_enabled(!b).unwrap();
+    }
   };
   let toggle_fn_clone = toggle_fn.clone();
 
   app.listen_global("login", move |_ev| {
-    toggle_fn(false);
+    toggle_fn(true);
   });
   app.listen_global("logout", move |_ev| {
-    toggle_fn_clone(true);
+    toggle_fn_clone(false);
   });
 }
