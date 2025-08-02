87ab2fbcf440a952335573bce46f2e5fc146aed2
Dan Knights <danknights95@gmail.com>
2025-08-02 23:33:21 +0100

    test(sync): add test to ensure note isn't overwritten on sync after deleting all content

--- src/__tests__/src/store/note/operations/editNote.test.ts
+++ src/__tests__/src/store/note/operations/editNote.test.ts
@@ -46,6 +46,39 @@ describe('editNote', () => {
     assert.isTrue(calls.invoke.has('edit_note'));
   });
 
+  it('Sets new unsynced note if empty to prevent sync clear', async () => {
+    const { calls } = mockApi();
+    const debounceSyncSpy = vi.spyOn(a, 'debounceSync');
+
+    await n.getAllNotes();
+
+    const currentSelectedNote = { ...n.noteState.selectedNote };
+    const noteToEdit = { ...n.findNote(n.noteState.selectedNote.uuid)! };
+
+    vi.clearAllMocks();
+    clearMockApiResults({ calls });
+
+    await waitForAutoSync(() => n.editNote({ ops: [{ insert: '' }] }, '', ''), calls);
+
+    expect(debounceSyncSpy).toHaveBeenCalledOnce();
+
+    const editedNote = n.findNote(noteToEdit.uuid)!;
+
+    assert.strictEqual(s.syncState.unsyncedNotes.size, 0);
+    assert.strictEqual(s.syncState.unsyncedNotes.new, noteToEdit.uuid);
+    assert.notDeepEqual(n.noteState.selectedNote, currentSelectedNote);
+    assert.notDeepEqual(editedNote, noteToEdit);
+    // See `editNote` for why selectedNote content should remain the same
+    assert.deepEqual(n.noteState.selectedNote.content, currentSelectedNote.content);
+    assert.notStrictEqual(
+      n.noteState.selectedNote.timestamp,
+      currentSelectedNote.timestamp
+    );
+    assert.notStrictEqual(editedNote.timestamp, noteToEdit.timestamp);
+    assert.strictEqual(calls.size, 1);
+    assert.isTrue(calls.invoke.has('edit_note'));
+  });
+
   it('Catches errors', async () => {
     const { calls, setErrorValue } = mockApi();
     const consoleErrorSpy = vi.spyOn(console, 'error');
