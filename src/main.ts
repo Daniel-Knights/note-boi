f1e9ca778745cb8fcd6a86063c5943d92ebf0e2f
Dan Knights <danknights95@gmail.com>
2025-11-07 00:00:31 +0000

    feat: remove sync on app exit
    syncing is handled on the fly, so the only edge case is if the user edits a note and closes the app before the sync completes. in this case, the edited note will be re-synced next time the app loads anyway

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
-import * as dialog from '@tauri-apps/plugin-dialog';
 import { WebviewWindow } from '@tauri-apps/api/webviewWindow';
 import { exit, relaunch } from '@tauri-apps/plugin-process';
 import 'quill/dist/quill.snow.css';
 import { createApp } from 'vue';
 
-import { debounceSync, deleteAccount, sync } from './api';
-import { ERROR_CODE } from './classes';
+import { debounceSync, deleteAccount } from './api';
 import { initLogger } from './log';
 import './sass/style.scss';
 import {
@@ -18,7 +16,6 @@ import {
   noteState,
 } from './store/note';
 import { openedPopup, POPUP_TYPE } from './store/popup';
-import { syncState } from './store/sync';
 import { handleUpdate } from './store/update';
 import { isDev, tauriInvoke, tauriListen } from './utils';
 
@@ -34,14 +31,12 @@ getAllNotes().then(() => {
   debounceSync(true);
 });
 
-webview.onCloseRequested(() => {
-  exitApp(async () => {
-    await tauriInvoke('backup_notes', { notes: noteState.notes }).catch((err) => {
-      console.error('Failed to backup notes:', err);
-    });
-
-    exit();
+webview.onCloseRequested(async () => {
+  await tauriInvoke('backup_notes', { notes: noteState.notes }).catch((err) => {
+    console.error('Failed to backup notes:', err);
   });
+
+  exit();
 });
 
 webview.onDragDropEvent((ev) => {
@@ -50,7 +45,11 @@ webview.onDragDropEvent((ev) => {
 
 tauriListen('reload', () => {
   // Relaunch acts up in dev, but is fine in production
-  exitApp(isDev() ? window.location.reload.bind(window.location) : relaunch);
+  if (isDev()) {
+    window.location.reload();
+  } else {
+    relaunch();
+  }
 });
 
 tauriListen('new-note', () => newNote(true));
@@ -69,26 +68,3 @@ tauriListen('delete-account', deleteAccount);
 tauriListen('change-password', () => {
   openedPopup.value = POPUP_TYPE.CHANGE_PASSWORD;
 });
-
-export async function exitApp(cb: () => void): Promise<void> {
-  if (syncState.unsyncedNotes.size > 0) {
-    await sync();
-
-    if (syncState.appError.code === ERROR_CODE.SYNC) {
-      const closeAnyway = await dialog.ask(
-        'ERROR: Failed to sync notes.\nClose anyway?',
-        {
-          title: 'NoteBoi',
-          kind: 'error',
-        }
-      );
-
-      if (closeAnyway) cb();
-      else openedPopup.value = POPUP_TYPE.ERROR;
-
-      return;
-    }
-  }
-
-  cb();
-}
