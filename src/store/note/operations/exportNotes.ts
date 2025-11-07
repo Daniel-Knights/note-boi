20e7e1c9cd45a2d9f0e7eb461ec93b39157dcfa2
Dan Knights <danknights95@gmail.com>
2025-11-07 18:08:58 +0000

    feat(export): web version of notes exporting

--- src/store/note/operations/exportNotes.ts
+++ src/store/note/operations/exportNotes.ts
@@ -1,19 +1,55 @@
 import * as tauriDialog from '@tauri-apps/plugin-dialog';
+import { AsyncZippable, strToU8, zip } from 'fflate';
 
-import { tauriInvoke } from '../../../utils';
+import { isDesktop, tauriInvoke } from '../../../utils';
 import { noteState } from '../state';
 
 /** Exports all notes, or a given selection. */
 export async function exportNotes(noteUuids: string[]): Promise<void> {
-  const saveDir = await tauriDialog.open({
-    title: 'Choose a location',
-    directory: true,
-    multiple: false,
-    recursive: false,
-  });
-  if (!saveDir) return;
-
   const notes = noteState.notes.filter((nt) => noteUuids.includes(nt.uuid));
 
-  tauriInvoke('export_notes', { saveDir, notes });
+  if (isDesktop()) {
+    const saveDir = await tauriDialog.open({
+      title: 'Choose a location',
+      directory: true,
+      multiple: false,
+      recursive: false,
+    });
+    if (!saveDir || typeof saveDir !== 'string') return;
+
+    tauriInvoke('export_notes', { saveDir: saveDir ?? '', notes });
+
+    return;
+  }
+
+  //// Web
+
+  const folderName = `note-boi-notes-${Date.now()}`;
+  const zippableNotes = notes.reduce((acc, nt) => {
+    acc[`${folderName}/${nt.uuid}.txt`] = strToU8(nt.getText());
+
+    return acc;
+  }, {} as AsyncZippable);
+
+  return new Promise((res, rej) => {
+    zip(zippableNotes, (err, data) => {
+      if (err) rej(err);
+
+      const blob = new Blob([new Uint8Array(data)], { type: 'application/zip' });
+      const url = URL.createObjectURL(blob);
+      const link = document.createElement('a');
+
+      link.href = url;
+      link.download = `${folderName}.zip`;
+
+      document.body.appendChild(link);
+
+      link.click();
+      link.remove();
+
+      URL.revokeObjectURL(url);
+
+      res();
+    });
+  });
 }
