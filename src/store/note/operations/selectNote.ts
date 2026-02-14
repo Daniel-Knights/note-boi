f5870aabc011da9471f48bd4b39a38e8cc2c94d1
Dan Knights <danknights95@gmail.com>
2026-02-14 23:48:12 +0000

    refactor(note): remove unused selectNote return values

--- src/store/note/operations/selectNote.ts
+++ src/store/note/operations/selectNote.ts
@@ -6,18 +6,16 @@ import { clearEmptyNote, findNote } from '../utils';
  * Looks for note with given `uuid` in {@link noteState.notes},
  * and sets it to {@link noteState.selectedNote}.
  */
-export function selectNote(uuid?: string): boolean {
-  if (noteState.selectedNote.uuid === uuid) return false;
+export function selectNote(uuid?: string) {
+  if (noteState.selectedNote.uuid === uuid) return;
 
   clearEmptyNote();
 
   const foundNote = findNote(uuid);
-  if (!foundNote) return false;
+  if (!foundNote) return;
 
   noteState.selectedNote = foundNote.clone();
 
   dispatchNoteEvent('note-select');
   dispatchNoteEvent('note-change');
-
-  return true;
 }
